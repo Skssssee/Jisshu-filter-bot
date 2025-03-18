d5a68a1cffcdc4f56809e85c2894d05e3662567d
Sarif167 <wajsarif461@gmail.com>
2025-03-18 07:29:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/cc3DV69V/4e8dfd2f1a59.jpg https://i.ibb.co/cc3DV69V/4e8dfd2f1a59.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/cc3DV69V/4e8dfd2f1a59.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
+PAYPICS = (environ.get('PAYPICS', 'https://i.ibb.co/S48z8FJr/90ec2f4e08e0.jpg')).split()
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
