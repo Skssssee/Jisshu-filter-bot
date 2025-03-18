ed4f67feaf58befc76c90a240ffc812f547a9bb2
Sarif167 <wajsarif461@gmail.com>
2025-03-18 08:25:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://quintessential-sibylle-owajbhai-8eceeee8.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
